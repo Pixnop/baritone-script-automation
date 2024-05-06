62e1b40fefdae0306400a954c670ce2334293dd7
leon fievet <fievet.leon@free.fr>
2024-05-06 22:28:46 +0200

    push

--- src/main/java/com/pixnop/mixin/ExampleMixin.java
+++ src/main/java/com/pixnop/mixin/ExampleMixin.java
@@ -1,5 +1,7 @@
 package com.pixnop.mixin;
 
+import baritone.api.BaritoneAPI;
+import com.pixnop.Baritonescriptautomation;
 import net.minecraft.server.MinecraftServer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -11,5 +13,6 @@ public class ExampleMixin {
 	@Inject(at = @At("HEAD"), method = "loadWorld")
 	private void init(CallbackInfo info) {
 		// This code is injected into the start of MinecraftServer.loadWorld()V
+
 	}
 }
